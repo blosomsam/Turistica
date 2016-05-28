0b06569d72dd341fc3f328e16538b23d92d932bf
Sandro Castillo J <blosomsam@gmail.com>
2016-05-27 22:48:36 -0500

    Desarrollo - modificando main

--- GuiaTuristica/src/guiaturistica/GuiaTuristica.java
+++ GuiaTuristica/src/guiaturistica/GuiaTuristica.java
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package guiaturistica;
 
 /**
- *
  * @author sandrojc
  */
 public class GuiaTuristica {
@@ -15,7 +9,6 @@ public class GuiaTuristica {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        
+    } 
 }
